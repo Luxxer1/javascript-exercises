b3f5073054dfe611a5fa6b6922fdeab795f84646
Lucas Bastos <lfsouzabastos@gmail.com>
2024-10-30 11:27:35 -0300

    finish exercise 07 - temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let formula = (f - 32) * (5/9);
+  return +formula.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let formula = (c * (9/5)) + 32;
+  return +formula.toFixed(1);
 };
 
 // Do not edit below this line
