e3b7d5d814e2a305e2c0d11e08e28c54bf5346a6
Lucas Bastos <lfsouzabastos@gmail.com>
2024-10-30 11:00:12 -0300

    finish ex 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  if(
+    !Number.isInteger(n1) || 
+    !Number.isInteger(n2) ||
+    n1 < 0 ||
+    n2 < 0
+  ) {
+    return "ERROR"
+  }
 
+  let sum = 0;
+  let i = (n1 > n2) ? n2 : n1;
+  let range = Math.abs(n1 - n2) + 1;
+
+  do {
+    sum += i;
+    i++;
+  } while (i <= range)
+
+  return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
