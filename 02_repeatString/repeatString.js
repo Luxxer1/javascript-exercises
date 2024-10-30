43accb8bdbb73058bd819b9b3367c8956ce9bf42
Lucas Bastos <lfsouzabastos@gmail.com>
2024-10-30 08:16:20 -0300

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+  
+  if (n < 0) {
+    return "ERROR"
+  }
 
+  let stringOutputed = "";
+  for (let i = 0; i < n; i++) {
+    stringOutputed += string;
+  }
+
+  return stringOutputed;
 };
 
 // Do not edit below this line
